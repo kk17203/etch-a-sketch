5e586fba6e37121b7d836a890c4e83bd32a02018
kk17203 <kk17203@gmail.com>
2023-05-21 22:14:18 -0500

    Add function to generate grid. Add css styling.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#container');
+
+grid(256);
+
+function grid(num) {
+    const containerWidth = container.offsetWidth;
+    const containerHeight = container.offsetHeight;
+    const boxSize = Math.floor(containerWidth/Math.sqrt(num));
+
+    for (r = 0; r < num; r++) {
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('gridBox');
+        gridBox.style.width = boxSize + 'px';
+        gridBox.style.height = boxSize + 'px';
+        container.appendChild(gridBox);
+    }
+}
+
