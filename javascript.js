2a3bfd0e5f8c972b94008f9b939f485697c05a8b
kk17203 <kk17203@gmail.com>
2023-05-22 16:03:37 -0500

    Add default gridSize. Add prompt to clear existing div grid and make new one using user input.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,39 @@
 const container = document.querySelector('#container');
+const btn = document.querySelector(".btn");
+let defaultGridSize = ("256");
+let gridSize = parseInt(defaultGridSize);
 
-grid(256);
+grid(gridSize);
+newGrid();
 
 function grid(num) {
     const containerWidth = container.offsetWidth;
     const containerHeight = container.offsetHeight;
     const boxSize = Math.floor(containerWidth/Math.sqrt(num));
-
     for (r = 0; r < num; r++) {
         let gridBox = document.createElement('div');
         gridBox.classList.add('gridBox');
         gridBox.style.width = boxSize + 'px';
         gridBox.style.height = boxSize + 'px';
         gridBox.addEventListener("mouseover", function(event) {
-            gridBox.style.backgroundColor = 'red';
+            gridBox.style.backgroundColor = 'yellow';
         })
+        
         container.appendChild(gridBox);
     }
 }
+
+function newGrid() {
+    btn.addEventListener('click', function(e) {
+        let gridBoxes = document.querySelectorAll('.gridBox');
+        gridBoxes.forEach(function(box) {
+            container.removeChild(box);
+            });
+        userInput = prompt('Set grid size', '');
+        gridSize = parseInt(userInput);
+        grid(gridSize);
+        console.log(gridSize);
+    });
+}
+
+//error happens if larger than 1,024 boxes.
